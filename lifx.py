ace913e6aa17292d33edf34415cee23fa086f6a8
Elias <18127101+elias123tre@users.noreply.github.com>
2019-03-23 16:03:47 +0100

    Experimenting with GPIO inputs from PIR

--- lifx.py
+++ lifx.py
@@ -1,17 +1,28 @@
 import requests
+import RPi.GPIO as GPIO
+import time
 
 token = ""
-
-headers = {
-    "Authorization": "Bearer %s" % token,
-}
-
+label = "Taklampa"
 payload = {
-    "power": "on",
+    "power": "off",
     "duration": 1.5,
 }
 
-motion = 1
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)  # Read output from PIR motion sensor
+# GPIO.setup(3, GPIO.OUT)  # LED output pin
 
-if motion == 1:
-    response = requests.put('https://api.lifx.com/v1/lights/label:Taklampa/state', data=payload, headers=headers)
+while True:
+    i = GPIO.input(11)
+if i == 0:  # When output from motion sensor is LOW
+    print("No intruders"), i
+    # GPIO.output(3, 0)  # Turn OFF LED
+    time.sleep(0.1)
+
+elif i == 1:  # When output from motion sensor is HIGH
+    print("Intruder detected"), i
+    # GPIO.output(3, 1)  # Turn ON LED
+    # response = requests.put("https://api.lifx.com/v1/lights/label:" + label + "/state", data=payload, headers={"Authorization": "Bearer %s" % token,)
+    time.sleep(0.1)
