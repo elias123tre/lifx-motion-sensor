fe072e50d3fa9d5c5f94a668ae2a9a94fb9511fb
Elias <18127101+elias123tre@users.noreply.github.com>
2019-03-23 16:04:49 +0100

    Added while loop for continuous input

--- lifx.py
+++ lifx.py
@@ -1,28 +1,36 @@
+import time
+
 import requests
 import RPi.GPIO as GPIO
-import time
 
 token = ""
 label = "Taklampa"
-payload = {
+on = {
+    "power": "on",
+    "duration": 1.5,
+}
+off = {
     "power": "off",
     "duration": 1.5,
 }
 
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)  # Read output from PIR motion sensor
-# GPIO.setup(3, GPIO.OUT)  # LED output pin
 
 while True:
     i = GPIO.input(11)
-if i == 0:  # When output from motion sensor is LOW
-    print("No intruders"), i
-    # GPIO.output(3, 0)  # Turn OFF LED
-    time.sleep(0.1)
 
-elif i == 1:  # When output from motion sensor is HIGH
-    print("Intruder detected"), i
-    # GPIO.output(3, 1)  # Turn ON LED
-    # response = requests.put("https://api.lifx.com/v1/lights/label:" + label + "/state", data=payload, headers={"Authorization": "Bearer %s" % token,)
-    time.sleep(0.1)
+    if i == 0:  # When output from motion sensor is LOW
+        time.sleep(0.5)
+    elif i == 1:  # When output from motion sensor is HIGH
+        print("Intruder detected"), i
+        # GPIO.output(3, 1)  # Turn ON LED
+        offtimer = 0
+        response = requests.put("https://api.lifx.com/v1/lights/label:" + label +
+                                "/state", data=on, headers={"Authorization": "Bearer %s" % token, })
+        time.sleep(0.5)
+    while offtimer > timertemp:
+        response = requests.put("https://api.lifx.com/v1/lights/label:" + label +
+                                "/state", data=off, headers={"Authorization": "Bearer %s" % token, })
